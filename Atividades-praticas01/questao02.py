dab1181b8ee84e1b65e354c2530b9e480191e1d4
GracieleTi <gracieleti7@gmail.com>
2025-08-21 21:56:44 -0300

    Atividade pratica 01 - Questao 02

---
+++ Atividades-praticas01/questao02.py
@@ -0,0 +1,11 @@
+#Este programa soma dois valores inteiros
+
+#valores que serão somados
+numero1 = 12
+numero2 = 14
+
+#Calcula soma dos valores
+soma = numero1 + numero2
+
+#Exibe o resultado
+print(f"A soma dos números {numero1} e {numero2} é igual a {soma}.")
