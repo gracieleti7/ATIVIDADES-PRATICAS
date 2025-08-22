72f24ffc8a9ea345cb9437ef13f4b77584d4a6d5
GracieleTi <gracieleti7@gmail.com>
2025-08-21 22:02:23 -0300

    Atividade pratica 02 - Questao 04

---
+++ Atividades-praticas02/questao04.py
@@ -0,0 +1,9 @@
+distancia_percorrida = 300
+combustivel_gasto = 25
+
+cunsumo = distancia_percorrida / combustivel_gasto
+
+print("Dados de consumo: ")
+print("Distância percorrida:", distancia_percorrida, "km")
+print("Combustível gasto:", combustivel_gasto, "litros")
+print("Consumo médio:", round(cunsumo, 2), "km/l")
