6a4beffd2c72f0fa43bbe1cee237a8c39712a623
GracieleTi <gracieleti7@gmail.com>
2025-08-21 22:02:02 -0300

    Atividade pratica 02 - Questao 02

---
+++ Atividades-praticas02/questao02.py
@@ -0,0 +1,12 @@
+nome_produto = "camiseta"
+preco_original = 50.00
+porcentagem_desconto = 20
+
+valor_desconto = preco_original * (porcentagem_desconto / 100)
+preco_final = preco_original - valor_desconto
+
+print("Nome do produto:", nome_produto)
+print("Preço original: R$", preco_original)
+print("Porcentagem de desconto:", porcentagem_desconto, "%")
+print("Valor do desconto: R$", round(valor_desconto, 2))
+print("Preço final: R$", round (preco_final, 2))
